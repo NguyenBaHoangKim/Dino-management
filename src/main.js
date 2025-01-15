217c292b3caa042f4c36de2ed4c336f0dec4ec8b
NguyenBaHoangKim <baloctq@gmail.com>
2025-01-15 17:18:40 +0700

    deploy vercel

--- src/main.js
+++ src/main.js
@@ -5,17 +5,17 @@ import * as database from '#configs/database'
 import * as email from '#configs/email'
 import logger from '#configs/logger'
 import app from '#configs/server'
-import { Server as SocketIOServer } from 'socket.io';
+import { Server as SocketIOServer } from 'socket.io'
 
 const { port } = config
 const server = http.createServer(app)
 const io = new SocketIOServer(server, {
     cors: {
-      origin: '*',
+        origin: '*',
     },
-  });
+})
 
-  app.set('socketio', io);
+app.set('socketio', io)
 
 export default async function main() {
     try {
@@ -25,26 +25,18 @@ export default async function main() {
         process.exit(1)
     }
 }
-
-const users = {};
-
 io.on('connection', (socket) => {
-  logger.warn('A user connected: ' + socket.id);
-  socket.emit('message', 'Hello ' + socket.id);
+    logger.warn('A user connected: ' + socket.id)
+    socket.emit('message', 'Hello ' + socket.id)
 
-  socket.on('login', (data) => {
-    logger.warn('User ' + data.userId + ' connected');
-    // saving userId to object with socket ID
-    users[socket.id] = data.userId;
-  });
+    socket.on('login', (data) => {
+        logger.warn('User ' + data.userId + ' connected')
+    })
 
-  socket.on('disconnect', () => {
-    logger.warn('User ' + users[socket.id] + ' disconnected');
-    // remove saved socket from users object
-    delete users[socket.id];
-    logger.warn('Socket disconnected: ' + socket.id);
-  });
-});
+    socket.on('disconnect', () => {
+        logger.warn('Socket disconnected: ' + socket.id)
+    })
+})
 
 
 async function startServer() {
